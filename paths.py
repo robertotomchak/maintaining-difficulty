6d4b196a35cdd2f4c53ac36a60eb06fe74b141e5
robertotomchak <robertotomchak@gmail.com>
2026-03-24 09:05:58 -0300

    ADD paths info

---
+++ paths.py
@@ -0,0 +1,11 @@
+"""
+    Contains the import paths that all files require
+"""
+
+# where all datasets are
+PATH_DATASETS = "."
+PATH_RESULTS = "."
+PATH_RUN_EXPERIMENTS = "experiments_to_run.txt"
+PATH_EXPERIMENTS = "grid_search/"
+
+PATH_HEARTBEAT = "heartbeat.json"
